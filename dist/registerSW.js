a69b6ffa0d953b38f01678cd8ec3b061c64d171d
Mokshagnatej <mokshagnatejkalepalli@gmail.com>
2026-09-07 22:11:59 +0530

    feat: update build assets, add loading animation, and reconfigure Tailwind CSS

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/RailYatra-Next-Gen-IRCTC-Indian-Railways-Redesign/sw.js', { scope: '/RailYatra-Next-Gen-IRCTC-Indian-Railways-Redesign/' })})}
